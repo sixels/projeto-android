43820752d70800ac8183bb90154c103a422de80f
Sixels <sixels@protonmail.com>
2025-11-02 17:28:32 -0300

    Não voltar para a activity de login

--- app/src/main/java/com/example/menu/LoginActivity.kt
+++ app/src/main/java/com/example/menu/LoginActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.menu.databinding.ActivityLoginBinding
-import com.example.menu.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -23,6 +20,7 @@ class LoginActivity : AppCompatActivity() {
 
         binding.loginButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
         }
 
