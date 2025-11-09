3a6c058a730ec46fd7026d3d917641d98e389fa5
Julia <juliacard.winkler6@gmail.com>
2025-11-09 18:47:43 -0300

    Tela historico

--- app/src/main/java/com/example/appruido/ui/screens/HistoricoScreen.kt
+++ app/src/main/java/com/example/appruido/ui/screens/HistoricoScreen.kt
@@ -1,5 +1,7 @@
 package com.example.appruido.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
@@ -21,8 +24,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.*
+import com.example.appruido.R
+
 
 import androidx.compose.runtime.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,6 +39,7 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun HistoricoScreen() {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,11 +56,11 @@ fun HistoricoScreen() {
                  verticalArrangement = Arrangement.spacedBy(15.dp)
             ) {
                 Text( text = "Selecione o período que deseja:", fontSize = 18.sp)
-                dropdown_menu() //caixa de opções de periodo de tempo
+                Dropdown_menu() //caixa de opções de periodo de tempo
 
             }
-
         }
+        Grafico()
 
 
 
@@ -60,9 +70,31 @@ fun HistoricoScreen() {
 
 
 }
+
+@Composable
+ fun Grafico(){
+
+    val imageId = R.drawable.grafico_img
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(350.dp)
+            .clip(RoundedCornerShape(size = 15.dp))
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = imageId),
+            contentDescription = "Fundo do Gráfico",
+            modifier = Modifier.fillMaxSize(), // Para preencher o Box
+            contentScale = ContentScale.Fit // Para ajustar e cortar
+        )
+
+    }
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun dropdown_menu(){
+fun Dropdown_menu(){
 
     var isExpanded by remember {
         mutableStateOf(value = false)
@@ -98,19 +130,26 @@ fun dropdown_menu(){
                 text = {
                     Text(text = "Hoje")},
                 onClick = {
-                    periodo = "Periodo 1"
+                    periodo = "Hoje"
                     isExpanded = false
                 }
                 
             )
             DropdownMenuItem(
                 text = {
-                    Text(text = "Periodo 2")},
+                    Text(text = "Últimos 7 dias")},
                 onClick = {
-                    periodo = "Periodo 2"
+                    periodo = "Últimos 7 dias"
                     isExpanded = false
                 }
-
+            )
+            DropdownMenuItem(
+                text = {
+                    Text(text = "Último mês")},
+                onClick = {
+                    periodo = "Último mês"
+                    isExpanded = false
+                }
             )
         }
     }
